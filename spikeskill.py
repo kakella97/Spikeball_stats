8fa78ee1f12694e77c20a49b91ce75e1fc18e9b8
mmilosev <milan.milosevic.mm@gmail.com>
2020-03-16 23:01:44 -0700

    Update to only use file input. Update to sort data by elo.

--- spikeskill.py
+++ spikeskill.py
@@ -6,20 +6,24 @@ Created on Fri Mar 13 22:47:18 2020
 """
 import os.path
 import pprint
+import sys
+
+INITIAL_ELO = 1000
 
 def elo(elo_a, elo_b, score):
-    pa = 1/(1+10**((elo_b - elo_a)/400)) # EXPECTED SCORE
-    pb = 1 - pa # EXPECTED SCORE
-    K=50
-    
+    pa = 1 / (1 + 10**((elo_b - elo_a) / 400)) # EXPECTED PERCENT TEAM A WINS
+    pb = 1 - pa # EXPECTED PERCENT TEAM B WINS
+    K = 50
+
     if score[:2] > score[3:]: # IF TEAM A WINS
-        elo_a = elo_a + K*(1 - pa) 
-        elo_b = elo_b + K*(0 - pb) 
+        elo_a = elo_a + K * (1 - pa)
+        elo_b = elo_b + K * (0 - pb)
     else: # IF TEAM B WINS
-        elo_b = elo_b + K*(1 - pb) 
-        elo_a = elo_a + K*(0 - pa) 
-        
-    return [int(elo_a), int(elo_b)]
+        elo_b = elo_b + K * (1 - pb)
+        elo_a = elo_a + K * (0 - pa)
+
+    return [elo_a, elo_b]
+
 
 def P_elo(teams,players):
     elos=[]
@@ -27,7 +31,7 @@ def P_elo(teams,players):
         for t in teams.keys():
             if p in t:
                 elos.append(teams[t][4])
-        players[p][4]=int((sum(elos))/len(elos))
+        players[p][4]= sum(elos) / len(elos)
         elos=[]
     return players
 
@@ -50,14 +54,14 @@ def input_stats(team, p_dict, score, team_dict):
         r=1
             
     if (not team_check(team[:2],team_dict)) and (not team_check(team[3:],team_dict)):
-        team_dict[team[:2]] = [0,0,0,0,1000]
-        team_dict[team[3:]] = [0,0,0,0,1000]
+        team_dict[team[:2]] = [0, 0, 0, 0, INITIAL_ELO]
+        team_dict[team[3:]] = [0, 0, 0, 0, INITIAL_ELO]
                 
     elif (team_check(team[:2],team_dict) and not team_check(team[3:], team_dict)):
-        team_dict[team[3:]] = [0,0,0,0,1000]
+        team_dict[team[3:]] = [0, 0, 0, 0, INITIAL_ELO]
                
     elif team_check(team[3:],team_dict) and not team_check(team[:2],team_dict): 
-        team_dict[team[:2]] = [0,0,0,0,1000]
+        team_dict[team[:2]] = [0, 0, 0, 0, INITIAL_ELO]
     
     ''' ELOS FOR TEAMS 1 AND 2 '''     
     
@@ -146,10 +150,9 @@ def input_stats(team, p_dict, score, team_dict):
             team_dict[team[3:][2::-1]][3] = team_dict[team[3:][2::-1]][0]/team_dict[team[3:][2::-1]][1]
     
     for i in [0,1,3,4]:
-        
         p=team[i]
         if team[i] not in p_dict: ### Not in dict ###
-            p_dict[p] = [0,0,0,0,1000]
+            p_dict[p] = [0, 0, 0, 0, INITIAL_ELO]
             
             if i < 2: ###first 2 players###
                 p_dict[p][0]+=l # wins
@@ -186,50 +189,42 @@ def input_stats(team, p_dict, score, team_dict):
                     p_dict[p][3] = p_dict[p][0]/p_dict[p][1] # win loss ratio
                 
     return [p_dict, team_dict]
-            
-            
-            
-print("Add data or stats?")
-decision = input('Add data or stats >')
 
 
-if decision =='Add data' or decision=='data':
+# last argument is file name
+fname = sys.argv[-1]
+with open(fname, "r") as f:
+    content = [line.strip() for line in f] # line.rstrip() returns a copy of the string with trailing characters stripped
+f.close()
 
-    fname = input(' Enter filename > ')
-    answer='Y'
-    while answer=='Y':
-        print('What are the teams? Enter games in order that they are played. >')
-        teams = input('AB-CD >')
-        print("Score?")
-        score = input('XX-XX >')
+for j in range(len(content)):
+    if content[j]=='':
+        del content[j:]
+        break
+teams={}
+players={}
+standings=[]
+i=0
+while i < len(content):
+    [players, teams] = input_stats(content[i], players, content[i+1], teams)
+    i+=2
     
-        if os.path.exists(fname):
-            with open(fname, "a") as f:
-                f.writelines(teams+'\n'+score+'\n')
-            f.close()
-        print("Would you like to continue?")
-        answer=input("Y or N >")
-    
-        
-        
-if decision=='stats':
-    fname=input('Filename >')
-    with open(fname, "r") as f:
-        content = [line.strip() for line in f] # line.rstrip() returns a copy of the string with trailing characters stripped
-    f.close()
+players = P_elo(teams, players)    
 
-    for j in range(len(content)):
-        if content[j]=='':
-            del content[j:]
-            break
-    teams={}
-    players={}
-    standings=[]
-    i=0
-    while i < len(content):
-        [players, teams] = input_stats(content[i], players, content[i+1], teams)
-        i+=2
-        
-    players = P_elo(teams, players)    
-    
-    pprint.pprint([teams, players])
+# round team elo to 3 decimal places
+for t in teams.keys():
+    teams[t][3] = round(teams[t][3], 3)
+    teams[t][4] = round(teams[t][4], 3)
+
+# round individual elo to 3 decimal places
+for p in players.keys():
+    players[p][3] = round(players[p][3], 3)
+    players[p][4] = round(players[p][4], 1)
+
+# sort by elo
+players = sorted(players.items(), key=lambda i: i[1][4], reverse=True)
+teams = sorted(teams.items(), key=lambda i: i[1][4], reverse=True)
+
+# pretty
+pprint.pp(teams)
+pprint.pp(players)
